6ba7f15770fa9aeece83c4678e825c6b92d84b5f
kkllow <161205285+kkllow@users.noreply.github.com>
2025-04-07 13:00:07 +0800

    Add files via upload

---
+++ lab4/qn2/Circle.java
@@ -0,0 +1,12 @@
+public class Circle implements Shape {
+    private int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return 3.14 * radius * radius;
+    }
+}
